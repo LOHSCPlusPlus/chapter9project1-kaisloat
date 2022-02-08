e214d80cc07e58b5b5509c178a6d8c2925032ace
KaiSLOAT <>
2022-02-08 17:45:23 +0000

    in progress...

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <fstream>
+struct CarType{
+  enum = {MAX_CHARACTERS = 50};
+  char Car Name[MAX_CHARACTERS];
+  double MPG;
+  int Cylinders;
+  double Displacement;
+  double Horsepower;
+  double Weight;
+  double Acceleration;
+  int Model;
+  char Origin[MAX_CHARACTERS];
+  bool ValidEntry;
+  CarType();
+};
+
+CarType::CarType(){
+
+}
+
+
+int main(){
+
+}
